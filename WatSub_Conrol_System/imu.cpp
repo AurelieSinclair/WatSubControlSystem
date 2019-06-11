183b8f0c262a126d385adc9ddf861c7fddc2527b
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2019-06-11 17:19:13 -0400

    imu fix

--- WatSub_Conrol_System/imu.cpp
+++ WatSub_Conrol_System/imu.cpp
@@ -8,22 +8,19 @@ void Imu::initialize() {
 void Imu::Update(){
   while (Serial1.available()) {
     data = Serial1.read();
-    if((counter == 0 && data == 0xAA) || (counter > 0 && counter < 19)){
+    if(((counter == 0 || counter == 1) && data == 0xAA) || (counter > 1 && counter < 19)){
       rx[counter] = data;
       counter++;
     } else {
+      if(counter >= 19){
+        yaw = (int)rx[4] << 8;
+        yaw |= rx[3];
+        pitch = (int)rx[6] << 8;
+        pitch |= rx[5];
+        roll = (int)rx[8] << 8;
+        roll |= rx[7];
+      }
       counter = 0;
-    }
-  }
-  if(counter == 19) {
-    yaw = (int)rx[4] << 8;
-    yaw |= rx[3];
-    pitch = (int)rx[6] << 8;
-    pitch |= rx[5];
-    roll = (int)rx[8] << 8;
-    roll |= rx[7];
-    
-    counter = 0;
   }
 }
 
