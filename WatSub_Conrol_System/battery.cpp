a55e0a1dd58936f5094da566ac4f6c9aa6da9d88
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2019-06-20 19:40:28 -0400

    battery updated

--- WatSub_Conrol_System/battery.cpp
+++ WatSub_Conrol_System/battery.cpp
@@ -6,12 +6,18 @@
 
 Battery::Battery()
 {
-
 }
 
-void Battery::get_SOC(int highPin, int groundPin)
+void Battery::readVoltage(float v)
 {
-  
+  pinVoltage = v * (5 / 1023.0);
+  realVoltage = pinVoltage * 11;
+}
+
+//Returns the battery charge in percent (0 - 100) ideally
+int Battery::get_SOC()
+{
+  return 100 * (FULL_CHARGE - realVoltage) / (FULL_CHARGE - EMPTY_CHARGE);
 }
 int Battery::get_life(){
   return 100;//placeholder  
--- WatSub_Conrol_System/battery.h
+++ WatSub_Conrol_System/battery.h
@@ -5,15 +5,24 @@
 #ifndef Battery_h
 #define Buoy_h//what?
 
-
+#define FULL_CHARGE 29.5
+#define EMPTY_CHARGE 19.0
 
 class Battery
 {
   public:
     Battery();
+<<<<<<< Updated upstream
     void get_SOC(int highPin, int groundPin);
     int get_life();
+=======
+    void readVoltage(float v);
+    int get_SOC();
+
+>>>>>>> Stashed changes
   private:
+    float pinVoltage = 0;
+    float realVoltage = 0;
 };
 
 #endif
