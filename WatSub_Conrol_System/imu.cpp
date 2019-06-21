7a62fddf535a6bc8ac17341ef026c7cb7f49f640
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2019-06-21 14:15:06 -0400

    Updated IMU, added IMU library

--- WatSub_Conrol_System/imu.cpp
+++ WatSub_Conrol_System/imu.cpp
@@ -1,19 +1,57 @@
 #include "Imu.h"
 
 void Imu::initialize() {
-  Wire.begin(); // Initiate the Wire library
+  Wire.begin();
+  myIMU.begin();
+  Wire.setClock(400000); //Increase I2C data rate to 400kHz
+  myIMU.enableRotationVector(100); //Send data update every 100ms
 }
 
 void Imu::Update(){
- 
+  //Look for reports from the IMU
+  if (myIMU.dataAvailable() == true)
+  {
+    float x = myIMU.getQuatI() - offsetX;
+    float y = myIMU.getQuatJ() - offsetY;
+    float z = myIMU.getQuatK() - offsetZ;
+    float w = myIMU.getQuatReal() - offsetW;
+
+    if(i == 2){
+      offsetOrientation();
+    }
+    if(i < 3){
+      i++;
+    }
+
+    float siny = 2 * (w * z + x * y);
+    float cosy = 1 - 2 * (y * y + z * z);
+    yaw = atan2(siny, cosy);
+    
+    float sinp = 2 * (w * y - z * x);
+    if(fabs(sinp) >= 1) {
+      pitch = copysign(M_PI / 2, sinp);
+    }
+    pitch = asin(sinp);
+    
+    float sinr = 2 * (w * x + y * z);
+    float cosr = 1 - 2 * (x * x + y * y);
+    roll = atan2(sinr, cosr);
+  }
 }
 
-int Imu::get_pitch(){
-  return pitch/100;
+void Imu::offsetOrientation(){
+  offsetX = myIMU.getQuatI();
+  offsetY = myIMU.getQuatJ();
+  offsetZ = myIMU.getQuatK();
+  offsetW = myIMU.getQuatReal();
 }
-int Imu::get_yaw(){
-  return yaw/100;
+
+float Imu::get_pitch(){
+  return pitch / 100;
 }
-int Imu::get_roll(){
-  return roll/100;
+float Imu::get_yaw(){
+  return yaw / 100;
+}
+float Imu::get_roll(){
+  return roll / 100;
 }
--- WatSub_Conrol_System/imu.h
+++ WatSub_Conrol_System/imu.h
@@ -1,29 +1,23 @@
 #ifndef IMU_H
 #define IMU_H
-#define RX_SIZE 19
-#include "Wire.h"
 
-#define IMU_ADDRESS 0x4B
+#include <Wire.h>
+#include "SparkFun_BNO080_Arduino_Library.h"
 
 class Imu {
 	private:
-    uint16_t yaw, pitch, roll;
-    uint16_t counter;
-    char rx[RX_SIZE];
-    char data;
-     
+    uint16_t yaw = 0, pitch = 0, roll = 0;
+    uint16_t i = 0;
+    float offsetX = 0, offsetY = 0, offsetZ = 0, offsetW = 0;
+    BNO080 myIMU;
+    void offsetOrientation();
+    
 	public:
-   Imu() {
-    yaw = 0;
-    pitch = 0;
-    roll = 0;
-    counter = 0;
-   }
    void initialize();
    void Update();
-   int get_pitch();
-   int get_yaw();
-   int get_roll();
+   float get_pitch();
+   float get_yaw();
+   float get_roll();
 };
 
 
